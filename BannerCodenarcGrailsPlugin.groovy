926eb9df2665321b3919290c06b39e51dc35ab0a
Bob Rullo <robert.rullo@sungardhe.com>
2010-12-07 13:29:41 -0500

    Creating 0.1-SNAPSHOT

--- BannerCodenarcGrailsPlugin.groovy
+++ BannerCodenarcGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class BannerCodenarcGrailsPlugin {
     // the plugin version
-    def version = "0.1.0"
+    def version = "0.1-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.5 > *"
     // the other plugins this plugin depends on
