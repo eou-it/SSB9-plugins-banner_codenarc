55ad67c820f3d9bc02919df2f59ff3f159946814
RajanandPK <rajanandppk@ellucian.com>
2015-06-25 09:47:50 +0530

    HRU-5985 enabling coverage grails plugin for banner codenarc plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -35,6 +35,10 @@ grails.project.dependency.resolution = {
         compile 'org.grails.plugins:codenarc:0.21'
         runtime  ":hibernate:3.6.10.10"
         compile ":tomcat:7.0.52.1"
+        test ':code-coverage:2.0.3-2',
+        {
+            excludes 'xercesImpl'
+        }
     }
 
 }
