f80a0d53d0382a9196f35bad1428e5c0ca565029
Raj <rajanand.pk@ellucian.com>
2015-09-07 11:39:36 +0530

    HRU-6084 Upgrading development mode dependency for Tomcat to 8.0.22 from 7.0.52.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -31,7 +31,7 @@ grails.project.dependency.resolution = {
     plugins {
         compile 'org.grails.plugins:codenarc:0.21'
         compile  ":hibernate:3.6.10.19"
-        compile ":tomcat:7.0.52.1"
+        compile ":tomcat:8.0.22"
         test ':code-coverage:2.0.3-3'
     }
 
