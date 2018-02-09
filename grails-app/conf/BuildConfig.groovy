f90c38a2d8f84c320b212a85beeb36282074f88b
Eshaan_kumar <eshaan.kumar@ellucian.com>
2018-02-09 11:10:31 +0530

    HRU-7611 Saml Security vulernabilties

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,5 @@
 /*******************************************************************************
-Copyright 2009-2012 Ellucian Company L.P. and its affiliates.
+Copyright 2009-2018 Ellucian Company L.P. and its affiliates.
 *******************************************************************************/
 
 grails.project.class.dir = "target/classes"
@@ -22,17 +22,24 @@ grails.project.dependency.resolution = {
         if (System.properties['PROXY_SERVER_NAME']) {
             mavenRepo "${System.properties['PROXY_SERVER_NAME']}"
         }
+        grailsPlugins()
+        grailsHome()
         grailsCentral()
         mavenCentral()
         mavenRepo "http://repository.jboss.org/maven2/"
 
+    }
+    dependencies {
+        runtime 'xalan:xalan:2.7.2'
     }
 
     plugins {
         compile 'org.grails.plugins:codenarc:0.21'
         compile  ":hibernate:3.6.10.19"
         compile ":tomcat:8.0.22"
-        test ':code-coverage:2.0.3-3'
+        test (':code-coverage:2.0.3-3'){
+            export = false
+        }
     }
 
 }
